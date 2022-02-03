ffc0fa72e1e192284d13de7ee03bfaa9883418a7
VK Programming Web <67411213+virendra2000@users.noreply.github.com>
2022-02-03 11:19:40 +0530

    Create Assignment3.py

---
+++ Assignment3.py
@@ -0,0 +1,3 @@
+mrks = {"Data Structure" : "95","Java" : "97","Digital Logic & Computer Architecture" : "97","Discrete Structures & Graph Theory" : "93","Engineering Mathematics" : "85"}
+max_mrks = max(mrks,key=mrks.get)
+print(max_mrks)
